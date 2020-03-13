524d5d542600868950f54fb54a4a82c2193173fe
= <=>
2020-03-13 10:53:00 +0000

    Initialisation

---
+++ src/com/mkcodes/main/Main.java
@@ -0,0 +1,14 @@
+package com.mkcodes.main;
+
+public class Main extends Application {
+
+	public void start(Stage primaryStage) {
+
+	}
+
+	public static void main(String[] args) {
+
+		launch(args);
+	}
+
+}
