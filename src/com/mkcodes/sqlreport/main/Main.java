3c37c8dc204f65f04d31eee4b3ffdff63251b5a7
= <=>
2020-03-16 10:19:53 +0000

    Adding event handlers and button - no functionality

--- src/com/mkcodes/sqlreport/main/Main.java
+++ src/com/mkcodes/sqlreport/main/Main.java
@@ -1,24 +1,25 @@
-package com.mkcodes.main;
+package com.mkcodes.sqlreport.main;
 
 import java.util.Map;
 
 import javafx.application.Application;
 import javafx.event.Event;
 import javafx.event.EventHandler;
-import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.FlowPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application implements EventHandler {
 
 	private Scene scene;
 	private String title = "SQL Runner v0.0.1";
-	private FlowPane flowPaneRoot;
+	private BorderPane paneRoot;
 	private Button startButton;
 	private Map<Integer, String> sqlReturns;
+	private TextArea textArea;
 
 	@Override
 	public void start(Stage primaryStage) {
@@ -26,11 +27,22 @@ public class Main extends Application implements EventHandler {
 		startButton = new Button("Run the SQL!");
 		startButton.setOnAction(this);
 
+		textArea = new TextArea();
+
 		// Set up stage and add all the bumfs
-		flowPaneRoot = new FlowPane(Orientation.VERTICAL, 5, 5);
-		flowPaneRoot.setAlignment(Pos.CENTER);
-		flowPaneRoot.getChildren().add(startButton);
-		scene = new Scene(flowPaneRoot, 500, 500);
+//		flowPaneRoot = new FlowPane(Orientation.VERTICAL, 5, 5);
+		paneRoot = new BorderPane();
+//		paneRoot.setAlignment(Pos.CENTER);
+//		paneRoot.getChildren().add(textArea);
+//		paneRoot.setAlignment(textArea, Pos.CENTER);
+//		paneRoot.setCenter(startButton);
+		paneRoot.setTop(startButton);
+		paneRoot.setCenter(textArea);
+		textArea.setPrefSize(450, 450);
+		BorderPane.setAlignment(startButton, Pos.TOP_CENTER);
+		paneRoot.setPrefSize(500, 500);
+
+		scene = new Scene(paneRoot, 500, 500);
 		primaryStage.setTitle(title);
 		primaryStage.setScene(scene);
 		primaryStage.show();
@@ -38,14 +50,17 @@ public class Main extends Application implements EventHandler {
 
 	public static void main(String[] args) {
 
+//		setUpSQLParams();
+
 		launch(args);
 	}
 
 	@Override
 	public void handle(Event arg0) {
 
+		textArea.setText("Running. Please wait.");
 		// spin up threads
-
+//		sqlReturns = runSQL();
 		//
 	}
 
